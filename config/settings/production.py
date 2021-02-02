a574027ed1487425c54866e4457e5170068ce470
Oğuzhan Çelikarslan <oguzhancelikarslan@gmail.com>
2021-02-02 13:00:50 +0300

    production ayarları postgresql için yapılandırıldı

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from .base import *
 
-ALLOWED_HOSTS = ['www.oguzhancelikarslan.com', '127.0.0.1']
+ALLOWED_HOSTS = ['oguzhancelikarslan.com', '127.0.0.1', '3.15.25.170']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,8 +32,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'prod.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
